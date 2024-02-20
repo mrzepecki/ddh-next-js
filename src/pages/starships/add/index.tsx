db33e31a5b34a8c605a2de63b65439748150a43f
Michał Rzepecki <michal@vimar.dev>
2024-02-21 00:00:26 +0100

    feat: add page for adding starship

---
+++ src/pages/starships/add/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout';
+import StarshipsAdd from "@/components/starships-add";
+
+export default function AddStarships() {
+  return (
+    <Layout>
+      <StarshipsAdd />
+    </Layout>
+  );
+}
