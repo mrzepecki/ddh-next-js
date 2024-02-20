263cc67141ec38aad1605425fbb0285c56aa4604
Michał Rzepecki <michal@vimar.dev>
2024-02-20 14:53:13 +0100

    refactor: separated peoples item to single component

--- src/components/peoples-list.tsx
+++ src/components/peoples-list.tsx
@@ -1,22 +1,18 @@
-import { Box, Button,Typography } from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
 
-import { People } from '@/types/people';
+import {People} from '@/types/people';
+import PeoplesListItem from "@/components/peoples-list-item";
 
 interface Props {
   peoples: People[];
 }
 
-const PeoplesList = ({ peoples }: Props): JSX.Element => {
+const PeoplesList = ({peoples}: Props): JSX.Element => {
   return (
     <>
-      {peoples.map((x) => (
-        <Box key={x.name} sx={{display: 'flex', justifyContent: 'space-between'}}>
-          <Typography  variant="subtitle1">
-            {x.name}
-          </Typography>
-          {x.id && <Button variant="text" href={"/peoples/" + x.id}>See details</Button>}
-        </Box>
-      ))}
+      {peoples.map((x) =>
+        <PeoplesListItem id={x.id} name={x.name}/>
+      )}
     </>
   );
 };
